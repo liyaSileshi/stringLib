6a292661118b46c43b951025138787a13e5d2b41
liyaSileshi <liyasileshi@gmail.com>
2020-11-08 14:43:12 -0800

    prototyped

--- index.js
+++ index.js
@@ -1,84 +1,93 @@
 
 // Capitalize the first letter
 String.prototype.capitalize = function() {
-
   let first = this[0].toUpperCase()
   return first + this.slice(1)
 }
 
 // Capitalize all letters
-function allcaps(str) {
-  return str.toUpperCase()
+String.prototype.allcaps = function() {
+  return this.toUpperCase()
 }
+console.log('hello'.allcaps())
+
 
 // Capitalize the first letter of each word
-function capitalizeWords(str) {
+String.prototype.capitalizeWords = function() {
   //split the string on white space
-  let words = str.split(' ')
+  let words = this.split(' ')
   let upper = words.map((word) => {
-    // upperCase(word)
-    return upperCase(word)
+    return word.capitalize()
   })
   return upper.join(' ')
 }
+console.log('my name is liya'.capitalizeWords())
 
-function removeExtraSpaces(str) {
-  str.trim() //removes white space from beginning and ending
-  const strings = str.split(' ') //array of strings from str
-  let trimmedStrings = strings.filter((str) => {
+String.prototype.removeExtraSpaces = function() {
+  this.trim() //removes white space from beginning and ending
+  const strings = this.split(' ') //array of strings from str
+  let tokens = strings.filter((str) => {
     return str !== ''
   })
-  return trimmedStrings.join(' ') //return the joined string
+  return tokens.join(' ') //return the joined string
 }
+console.log('  ef  my name   is liya'.removeExtraSpaces())
 
-function kabobCase(str) {
-  str = str.toLowerCase()
-  str.trim() //removes white space from beginning and ending
-  const strings = str.split(' ') //array of strings from str
-  let trimmedStrings = strings.filter((str) => {
-    return str !== ''
-  })
-  return trimmedStrings.join('-') //return the joined string
+String.prototype.kabobCase = function() {
+  let tokens = cleanString(this)
+  return tokens.join('-') //return the joined string
 }
+console.log('  ef  my name   is liya'.kabobCase())
 
-function snakeCase(str) {
-  str = str.toLowerCase()
-  str.trim() //removes white space from beginning and ending
-  const strings = str.split(' ') //array of strings from str
-  let trimmedStrings = strings.filter((str) => {
-    return str !== ''
-  })
-  return trimmedStrings.join('_') //return the joined string
+
+String.prototype.snakeCase = function() {
+  let tokens = cleanString(this)
+  return tokens.join('_') //return the joined string
 }
+console.log('  ef  my name   is liya'.snakeCase())
 
-function camelCase(str) {
+String.prototype.camelCase = function() {
   // lowercase the first character of the word
-  str.trim() //remove white spaces from ends
-  let first = str[0].toLowerCase() //lowercase the first ch
-  str = first + str.slice(1) //connect with the rest of str
+  this.trim() //remove white spaces from ends
+  let first = this[0].toLowerCase() //lowercase the first ch
+  let str = first + this.slice(1) //connect with the rest of str
   
-  str = removeExtraSpaces(str) //remove any extra spaces
+  str = str.removeExtraSpaces() //remove any extra spaces
   const strings = str.split(' ') //array of string
 
   let rest = strings.slice(1)
    //capitalize first letter of each word
   let capitalized = rest.map((str) => {
-    return capitalize(str)
+    return str.capitalize()
   })
   let restWords = capitalized.join('')
   let result = strings[0] + restWords
   return result
 }
+console.log('  ef  my name   is liya'.camelCase())
 
 /*
 this method will take the first character of a 
 string and move to the end of a string:
 */
-function shift(str, num=1) {
+String.prototype.shift = function(num=1) {
+  let str = this
   for (let i = 1; i <= num; ++i) {
     let first = str[0]
     let rest = str.slice(1)
     str = rest + first
   }
   return str
+}
+console.log('liya'.shift(2))
+
+/* clean string : helper function for kabob and snake*/
+function cleanString(str) {
+  str = str.toLowerCase()
+  str.trim() //removes white space from beginning and ending
+  const strings = str.split(' ') //array of strings from str
+  let tokens = strings.filter((str) => {
+    return str !== ''
+  })
+  return tokens
 }
